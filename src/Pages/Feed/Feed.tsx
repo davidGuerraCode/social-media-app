ab7d82e5bc1b7785325b918b763cd9cc8916f910
David Vasquez <djv_guerra@hotmail.com>
2020-11-17 18:43:46 -0400

    Setup project

---
+++ src/Pages/Feed/Feed.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+import { Topbar } from '../../components/SocialMediaApp/components/Topbar';
+
+const Feed = () => {
+  return (
+    <div>
+      <Topbar />
+      <h1>Hi, this is the feed!</h1>
+    </div>
+  );
+};
+
+export default Feed;
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { SocialMediaApp } from './components/SocialMediaApp';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <SocialMediaApp />
   </React.StrictMode>,
   document.getElementById('root')
 );
